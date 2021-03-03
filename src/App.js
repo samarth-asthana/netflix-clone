f212d2846e673e5c043b43894b8285c0860fea0d
Samarth Asthana <samarthasthana110@gmail.com>
2021-03-03 16:42:24 +0530

    completed profile screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import HomeScreen from './screens/HomeScreen.js'
 import LoginScreen from './screens/LoginScreen.js'
+import { auth } from './firebase'
+import { useDispatch, useSelector } from 'react-redux'
+import { login, logout, selectUser } from './features/userSlice'
+import ProfileScreen from './ProfileScreen'
 function App() {
-  const user = null
+  const user = useSelector(selectUser)
+  const dispatch = useDispatch()
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        dispatch(
+          login({
+            uid: userAuth.uid,
+            email: userAuth.email,
+          })
+        )
+      } else {
+        dispatch(logout())
+      }
+    })
+    return unsubscribe
+  }, [dispatch])
   return (
     <div className="app">
       <Router>
@@ -12,6 +32,9 @@ function App() {
           <LoginScreen />
         ) : (
           <Switch>
+            <Route path="/profile">
+              <ProfileScreen />
+            </Route>
             <Route exact path="/">
               <HomeScreen />
             </Route>
